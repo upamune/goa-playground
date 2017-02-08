6ae927ac89a53c6c65b712198d40e39d3414806a
upamune <jajkeqos@gmail.com>
2017-02-08 14:49:58 +0900

    bootstrap

---
+++ README.md
@@ -0,0 +1,7 @@
+# goa-playground 
+
+---
+
+```
+$ goagen bootstrap -d github.com/upamune/goa-playground/design
+```
---
+++ operands.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/goadesign/goa"
+	"github.com/upamune/goa-playground/app"
+)
+
+// OperandsController implements the operands resource.
+type OperandsController struct {
+	*goa.Controller
+}
+
+// NewOperandsController creates a operands controller.
+func NewOperandsController(service *goa.Service) *OperandsController {
+	return &OperandsController{Controller: service.NewController("OperandsController")}
+}
+
+// Add runs the add action.
+func (c *OperandsController) Add(ctx *app.AddOperandsContext) error {
+	// OperandsController_Add: start_implement
+
+	// Put your logic here
+
+	// OperandsController_Add: end_implement
+	return nil
+}
